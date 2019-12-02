787416c01dbc47fbf0bd9ab557b45adcc5f3b662
Alexander Terry <alexterry43@gmail.com>
2019-12-01 21:55:23 -0900

    day1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import math
 
+
+def fuel_needed(module_mass: int) -> int:
+    w = math.floor(module_mass/ 3) - 2
+    # print(f"in: {module_mass}, w: {w}")
+    if w > 0:
+        return w + fuel_needed(w) or 0
+    return 0
+
+
 masses = open("input.txt", 'r')
 total = 0
 for i in masses.readlines():
-    mass = int(i)
-    fuel = math.floor(mass / 3) - 2
-    total += fuel
+    mass = fuel_needed(int(i))
+    total += mass
 
 print(total)
